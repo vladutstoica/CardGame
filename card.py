739a9f392aa5786f708d7ea0e5f8b8ffc45d0d19
iamSHTO <jokeerioro@gmail.com>
2019-11-18 23:10:05 +0200

    add comments

--- card.py
+++ card.py
@@ -1,10 +1,5 @@
-# TO DO
-# 3. import what we need not all library
-# 6. improve aspect / clean code
-
-
-from tkinter import *
-from cards import *
+# import tkinter for GUI and random for gambling
+from tkinter import Tk, Button, Label, Entry, PhotoImage, NORMAL, END, DISABLED, CENTER
 import random
 
 
@@ -14,7 +9,6 @@ class CardGame:
         master.title("Card Game")
         master.configure(background='white', padx=30, pady=30)
 
-        # import card images
         self.cardBackImg = PhotoImage(file="cards/card_back.png")
 
         self.cardRedImg = [
@@ -45,6 +39,7 @@ class CardGame:
             PhotoImage(file="cards/king_of_hearts.png"),
             PhotoImage(file="cards/king_of_diamonds.png")
         ]
+
         self.cardBlackImg = [
             PhotoImage(file="cards/ace_of_spades.png"),
             PhotoImage(file="cards/ace_of_clubs.png"),
@@ -76,16 +71,29 @@ class CardGame:
 
         # create card window
         self.cardWindow = Label(
-            master, image=self.cardBackImg, width=222, height=323, bg="white"
+            master,
+            image=self.cardBackImg,
+            width=222,
+            height=323,
+            bg="white",
+            borderwidth=1,
+            relief="solid"
         )
+
         # we need to keep a reference to the image in order to display it http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
         self.cardWindow.image = self.cardBackImg
-        self.cardWindow.grid()
+        self.cardWindow.grid(ipadx=5, ipady=5)
 
         # create RED button
         redButtonImg = PhotoImage(file="buttons/redButton.png")
         self.redButton = Button(
-            master, image=redButtonImg, bg="white", borderwidth=0, command=lambda: self.click("red")
+            master,
+            image=redButtonImg,
+            bg="white",
+            activebackground="red",
+            cursor="hand1",
+            borderwidth=0,
+            command=lambda: self.click("red")  # call click() function
         )
         self.redButton.image = redButtonImg
         self.redButton.grid()
@@ -93,7 +101,13 @@ class CardGame:
         # create BLACK button
         blackButtonImg = PhotoImage(file="buttons/blackButton.png")
         self.blackButton = Button(
-            master, image=blackButtonImg, bg="white", command=lambda: self.click("black")
+            master,
+            image=blackButtonImg,
+            bg="white",
+            activebackground="black",
+            borderwidth=0,
+            cursor="hand1",
+            command=lambda: self.click("black")
         )
         self.blackButton.image = blackButtonImg
         self.blackButton.grid()
@@ -105,7 +119,9 @@ class CardGame:
 
         # create CASH OUT button
         self.cashOut = Button(
-            master, text="CASH OUT", command=lambda: self.cashOutTrigger()
+            master,
+            text="CASH OUT",
+            command=lambda: self.cashOutTrigger()  # call cashOutTrigger() function
         )
         self.cashOut.grid(pady=20, ipady=10)
 
@@ -115,11 +131,13 @@ class CardGame:
         self.bet.insert(0, "BET")
 
         # create INFO
-        self.info = Entry(master,
-                          justify=CENTER,
-                          state=DISABLED,
-                          disabledbackground="orange",
-                          disabledforeground="white")
+        self.info = Entry(
+            master,
+            justify=CENTER,
+            state=DISABLED,
+            disabledbackground="orange",
+            disabledforeground="white"
+        )
         self.info.grid(column=1, ipadx=20)
 
         # make grid system
@@ -133,45 +151,99 @@ class CardGame:
                 components[count].grid(row=row, column=column)
                 count += 1
 
-        self.trigger = False
+        self.trigger = False  # it helps us leading the player and resolve some bugs
         self.credit.bind(
-            "<FocusIn>", lambda event: self.clear_placeholder(event, self.credit,))  # so i didnt get error about the parameters of the function
+            "<FocusIn>",
+            lambda event: self.clear_placeholder(event, self.credit,)
+        )  # when a player focus the text box, the text will disappear
         self.credit.bind(
-            "<FocusOut>", lambda event: self.add_placeholder(event, self.credit,  "CREDIT"))  # if you change placeholder be sure to change `def click()` ifs too
+            "<FocusOut>",
+            lambda event: self.add_placeholder(event, self.credit,  "CREDIT")
+        )  # when a player focus outside the text box, the text will appear if the string is 0 or empty
 
         self.bet.bind(
-            "<FocusIn>", lambda event: self.clear_placeholder(event, self.bet))
+            "<FocusIn>",
+            lambda event: self.clear_placeholder(event, self.bet)
+        )  # when a player focus the text box, the text will disappear
         self.bet.bind(
-            "<FocusOut>", lambda event: self.add_placeholder(event, self.bet, "BET"))  # if you change placeholder be sure to change `def click()` ifs too
-
-    # adds placeholder to the both entries
-    def add_placeholder(self, event, whatEntry, placeholder):
-        if not whatEntry.get() or whatEntry.get() == "0":
-            whatEntry.delete(0, END)
-            whatEntry.insert(0, placeholder)
-
-    # clean placeholder to the both entries
-    def clear_placeholder(self, event, whatEntry):
-        if whatEntry.get() == "CREDIT" or whatEntry.get() == "BET":
-            whatEntry.delete(0, END)
+            "<FocusOut>",
+            lambda event: self.add_placeholder(event, self.bet, "BET")
+        )  # when a player focus outside the text box, the text will appear if the string is 0 or empty
 
     def click(self, color):  # what happen when you click a color
+
+        # first checks if you insert CREDIT
         if len(self.credit.get()) == 0 or self.credit.get() == "CREDIT":
             print("Please insert coins first!")
             self.infoDisplay("Please insert coins first!", "orange")
+
+        # second checks if you insert BET
         elif len(self.bet.get()) == 0 or self.bet.get() == "BET":
             print("Please bet first!")
             self.infoDisplay("Please bet first!", "orange")
+
+        # third checks if BET is bigger than CREDIT and IF the trigger is OFF
+        # ex.: IF the trigger is ON and you win more hands in a row,
+        # when the BET get bigger than your CREDIT you can't play anymore and you are forced to CASH OUT
         elif int(self.bet.get()) > int(self.credit.get()) and self.trigger is False:
             print("You don't have enough credit!")
             self.infoDisplay("You don't have enough credit!", "orange")
         else:
-            chance = random.randint(1, 10)
-            print(chance)
+            chance = random.choice([True, False])
             self.takeCredit()
             self.flipCard(chance)
             self.checkWin(color, chance)
             self.disableBet()
+
+    def takeCredit(self):  # take coins from CREDIT when you BET your first hand
+        if self.trigger is False:  # without trigger condition you would be charged every time you bet even if you win
+            aux = int(self.credit.get())
+            self.credit.delete(0, END)
+            self.credit.insert(0, aux-int(self.bet.get()))
+
+    def flipCard(self, chance):  # change card according to the color
+        index = random.randint(0, 25)
+        if chance is True:
+            self.cardWindow.configure(image=self.cardRedImg[index])
+            self.cardWindow.image = self.cardRedImg[index]
+        elif chance is False:
+            self.cardWindow.configure(image=self.cardBlackImg[index])
+            self.cardWindow.image = self.cardBlackImg[index]
+
+    def checkWin(self, color, chance):  # verify if you win or lose
+        if color == "red" and chance is True:
+            print("You win!")
+            self.infoDisplay("You win!", "green")
+            self.bet.configure(state=NORMAL)
+            self.trigger = True
+            aux = int(self.bet.get())
+            self.bet.delete(0, END)
+            self.bet.insert(0, aux*2)
+            self.bet.configure(state=DISABLED)
+        elif color == "black" and chance is False:
+            print("You win!")
+            self.infoDisplay("You win!", "green")
+            self.bet.configure(state=NORMAL)
+            self.trigger = True
+            aux = int(self.bet.get())
+            self.bet.delete(0, END)
+            self.bet.insert(0, aux*2)
+            self.bet.configure(state=DISABLED)
+        else:
+            print("You lose!")
+            self.infoDisplay("You lose!", "tomato")
+            self.bet.configure(state=NORMAL)
+            self.bet.delete(0, END)
+            self.trigger = False
+
+    # disabled input so you can't increase your bet(winnings) before you CASH OUT
+    def disableBet(self):
+        if self.trigger is False:
+            print("false output")
+            self.bet.configure(state=NORMAL)
+        else:
+            print("true output")
+            self.bet.configure(state=DISABLED)
 
     def cashOutTrigger(self):  # take coins out
         if len(self.credit.get()) == 0 or len(self.bet.get()) == 0:
@@ -193,7 +265,7 @@ class CardGame:
             print("Try to win first!")
             self.infoDisplay("Try to win first!", "orange")
 
-    def infoDisplay(self, outputText, color):  # display some usefull information
+    def infoDisplay(self, outputText, color):  # display some usefull informations
         self.info.configure(state=NORMAL)  # so we can modify it
         self.info.delete(0, END)
         self.info.insert(0, outputText)
@@ -202,56 +274,16 @@ class CardGame:
             state=DISABLED  # return to the first state
         )
 
-    def takeCredit(self):  # when you bet take credit
-        if self.trigger is False:
-            aux = int(self.credit.get())
-            self.credit.delete(0, END)
-            self.credit.insert(0, aux-int(self.bet.get()))
+    # adds placeholder to the both entries (CREDIT and BET)
+    def add_placeholder(self, event, whatEntry, placeholder):
+        if not whatEntry.get() or whatEntry.get() == "0":
+            whatEntry.delete(0, END)
+            whatEntry.insert(0, placeholder)
 
-    def disableBet(self):  # so you cant edit bet after first shot
-        if self.trigger is False:
-            print("false output")
-            self.bet.configure(state=NORMAL)
-        else:
-            print("true output")
-            self.bet.configure(state=DISABLED)
-
-    def flipCard(self, chance):  # change card according to the color
-        # random choose one card from the lists
-        cardsRandom = random.randint(0, len(self.cardRedImg))
-        if 1 <= chance <= 5:
-            self.cardWindow.configure(image=self.cardRedImg[cardsRandom])
-            self.cardWindow.image = self.cardRedImg[cardsRandom]
-        elif 6 <= chance <= 10:
-            self.cardWindow.configure(image=self.cardBlackImg[cardsRandom])
-            self.cardWindow.image = self.cardBlackImg[cardsRandom]
-
-    def checkWin(self, color, chance):  # verify if is a win or a lose
-        if color == "red" and 1 <= chance <= 5:
-            print("You win!")
-            self.infoDisplay("You win!", "green")
-            self.bet.configure(state=NORMAL)
-            self.trigger = True
-            aux = int(self.bet.get())
-            self.bet.delete(0, END)
-            self.bet.insert(0, aux*2)
-            self.bet.configure(state=DISABLED)
-        elif color == "black" and 6 <= chance <= 10:
-            print("You win!")
-            self.infoDisplay("You win!", "green")
-            self.bet.configure(state=NORMAL)
-            self.trigger = True
-            aux = int(self.bet.get())
-            self.bet.delete(0, END)
-            self.bet.insert(0, aux*2)
-            self.bet.configure(state=DISABLED)
-        else:
-            print("You lose!")
-            self.infoDisplay("You lose!", "tomato")
-            self.bet.configure(state=NORMAL)
-            self.bet.delete(0, END)
-            self.trigger = False
-            self.bet.configure(state=DISABLED)
+    # clean placeholder to the both entries (CREDIT and BET)
+    def clear_placeholder(self, event, whatEntry):
+        if whatEntry.get() == "CREDIT" or whatEntry.get() == "BET":
+            whatEntry.delete(0, END)
 
 
 root = Tk()
